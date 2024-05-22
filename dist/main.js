09d052dfea5b1d18ca9a0fe65845c053c4f77c13
Jessica Perez Enrniquez <>
2024-05-22 23:01:09 +0200

    animate

--- dist/main.js
+++ dist/main.js
@@ -13,7 +13,68 @@ function closeSidebar() {
         sidebar.style.display = 'none';
     }, 300);
 }
+var AnimateCmp = {
+	controller: null,
+	init: function(){
+		AnimateCmp.controller = new ScrollMagic.Controller();
+		$(document).find(".cmp-animate").each(function(){
+			if($(this).find(".card-animate").length > 0){
+				AnimateCmp.basicAnimate($(this));
+			}
+			if($(this).find(".animate-fall").length > 0){
+				AnimateCmp.fallAnimate($(this));
+			}
+		});
+		$(document).find(".lca-banner-1").each(function(){
+			if($(this).find(".animate-text").length > 0){
+				AnimateCmp.effectText($(this));
+			}
+			if($(this).find(".animate-img").length > 0){
+				AnimateCmp.imgAnimate($(this));
+			}
+		});
+		$(document).find(".mosaic-component .block-container-img").each(function(){
+			AnimateCmp.mosaicAnimate($(this));
+		})
+	},
+	basicAnimate: function(obj){
+		var card = obj.find(".card-animate");
+		var time = obj.height() /2;
+		var tweenCard = TweenMax.from(card, 0.5,{transform: 'translate(0,40px)',ease: Power0.easeNone});
+		new ScrollMagic.Scene({triggerElement: obj[0], duration: time, reverse:false}) 
+			.setTween(tweenCard)
+			.addTo(AnimateCmp.controller);
+	},
+	fallAnimate: function(obj){
+		var tweenCard = TweenMax.staggerFromTo(obj.find(".animate-fall .grid-card-item .card, .animate-fall .grid-card-item .block-globo"), 0.5,{transform: 'translate(0,40px)',opacity: '0', ease: Power0.easeNone},{transform: 'translate(0,0)',opacity: '1', ease: Power0.easeNone}, 0.4);
+		new ScrollMagic.Scene({triggerElement: obj[0], reverse:false})
+			.setTween(tweenCard)
+			.addTo(AnimateCmp.controller);
+	},
+	imgAnimate: function(obj){
+		var img = obj.find(".animate-img")
+		var tweenCard = TweenMax.from(img, 1.5,{transform: 'scale(1.3)'});
+		new ScrollMagic.Scene({triggerElement: obj[0], reverse:false}) 
+			.setTween(tweenCard)
+			.addTo(AnimateCmp.controller);
+	},
+	effectText:function(obj) {
+		var objAnimate = obj.find(".animate-text");
+		var tweenCard = new TimelineMax().from(objAnimate, 1.5,{transform: 'translate(0px, 40px)',opacity: '0',delay:0.5});
+		new ScrollMagic.Scene({triggerElement: obj[0], reverse:false}) 
+			.setTween(tweenCard)
+			.addTo(AnimateCmp.controller);
+	},
+	mosaicAnimate: function(obj){
+		var time = obj.height() /2;
+		var tweenCard = TweenMax.from(obj, 0.5,{opacity: 0,ease: Power0.easeNone});
+		new ScrollMagic.Scene({triggerElement: obj[0], duration: time, reverse:false}) 
+			.setTween(tweenCard)
+			.addTo(AnimateCmp.controller);
+	}
+}
 $(document).ready(function(){
+	AnimateCmp.init();
 	if($(document).find(".component-carousel").length > 0){
 		initCompCarrousel();
 	}
@@ -41,6 +102,7 @@ $(document).ready(function(){
 			resize: true
 		});
 	}
+	
 	
 });
 function initGaleria(obj) {
